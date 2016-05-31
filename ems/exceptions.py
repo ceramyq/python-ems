91e065d2b1cd7602efceb85616fa7f53a4c6a9ab
Joshua Griffiths <jgriffiths@opsview.com>
2016-05-31 15:34:41 +0000

    Allow to create api exception from tuple
    Helps creating exceptions for V2 api resources which do
    not product an XML response for errors

--- ems/exceptions.py
+++ ems/exceptions.py
@@ -55,8 +55,12 @@ class EMSAPIException(EMSException):
     Exceptions pertaining to the Sentinel EMS API.
     """
     def __init__(self, ems_error):
-        self.code = ems_error.code
-        self.description = ems_error.description
+        if isinstance(ems_error, tuple):
+            self.code = ems_error[0]
+            self.description = ems_error[1]
+        else:
+            self.code = ems_error.code
+            self.description = ems_error.description
 
         message = "[%d] %s" % (ems_error.code, ems_error.description)
         super(EMSException, self).__init__(message)
